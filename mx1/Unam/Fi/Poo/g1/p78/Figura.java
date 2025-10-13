248a902bb1c39906472068a6c8ce2393fe36c583
Adrian Fernandez <adrianfmzm@gmail.com>
2025-10-12 21:29:55 -0600

    Elimino archivos

--- mx1/Unam/Fi/Poo/g1/p78/Figura.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Dimension;
-import java.awt.Graphics2D;
-
-public abstract class Figura {
-    public abstract double area();
-    public abstract double perimetro();
-    public abstract void dibujar(Graphics2D g, Dimension size);
-}
